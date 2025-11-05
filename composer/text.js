17932136b185a43a3a39c6350d9832c163f00b22
Whsxg <shabanov.matveyka@bk.ru>
2025-11-05 22:49:58 +0300

    Update text.js

--- composer/text.js
+++ composer/text.js
@@ -4,13 +4,20 @@ const { Composer, Markup } = require('telegraf');
 const composer = new Composer();
 
 composer.start(async (ctx) => {
+    return ctx.reply(
+        `Ниже представлены все команды для учебников всех классов/n /BooksClass10`,
+        { parse_mode: 'HTML' } // Доступ к reply_markup через свойство объекта
+    );
+});
+
+composer.BooksClass10(async (ctx) => {
     const buttons = Markup.inlineKeyboard([
-        [Markup.button.url('Go To Jobs', 'https://t.me/GreatsJobsBot/GreatsJobs')],
-        [Markup.button.url('Community', 'https://t.me/@whsxg0')],
+        [Markup.button.url('Алгебра', 'https://t.me/GreatsJobsBot/GreatsJobs')],
+        [Markup.button.url('Геометрия', 'https://t.me/@whsxg0')],
     ]);
 
     return ctx.reply(
-        `Looking for employees or a job in @GreatsJobsBot.`,
+        `Учебники за 10 Класс:`,
         { parse_mode: 'HTML', reply_markup: buttons.reply_markup } // Доступ к reply_markup через свойство объекта
     );
 });
