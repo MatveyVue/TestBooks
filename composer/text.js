d521afa76c405c2b15d3cab7b66f2b9da066e43c
Whsxg <shabanov.matveyka@bk.ru>
2025-11-05 22:59:54 +0300

    Update text.js

--- composer/text.js
+++ composer/text.js
@@ -1,25 +1,49 @@
-
 const { Composer, Markup } = require('telegraf');
 
 const composer = new Composer();
 
 composer.start(async (ctx) => {
-    return ctx.reply(
-        `Ниже представлены все команды для учебников всех классов/n /BooksClass10`,
-        { parse_mode: 'HTML' } // Доступ к reply_markup через свойство объекта
-    );
+    // Текст для команды /start
+    const startMessageText = `
+        Ниже представлены все команды для учебников всех классов:
+        <br>
+        /books_class_10 - Учебники за 10 класс
+        <br>
+        /books_class_11 - Учебники за 11 класс
+        <br>
+        (Добавьте другие команды по аналогии)
+    `;
+
+    // Отправляем сообщение для /start
+    try {
+        await ctx.reply(startMessageText, {
+            parse_mode: 'HTML',
+        });
+    } catch (error) {
+        console.error("Error sending /start message:", error);
+    }
 });
 
-composer.BooksClass10(async (ctx) => {
+// Обработка команды /books_class_10
+composer.command('books_class_10', async (ctx) => {
     const buttons = Markup.inlineKeyboard([
-        [Markup.button.url('Алгебра', 'https://kstu.kg/fileadmin/user_upload/algebra_10-11_kl_ja_alimov_.pdf')],
-        [Markup.button.url('Геометрия', 'https://t.me/@whsxg0')],
+        [Markup.button.url('Алгебра (10-11 класс)', 'https://kstu.kg/fileadmin/user_upload/algebra_10-11_kl_ja_alimov_.pdf')],
+        [Markup.button.url('Геометрия', 'https://t.me/@whsxg0')], // Уточните URL для геометрии, если это не канал
+        // Добавьте другие учебники по аналогии
     ]);
 
-    return ctx.reply(
-        `Учебники за 10 Класс:`,
-        { parse_mode: 'HTML', reply_markup: buttons.reply_markup } // Доступ к reply_markup через свойство объекта
-    );
+    const messageText = `
+        📚 Учебники за 10 Класс:
+    `;
+
+    try {
+        await ctx.reply(messageText, {
+            parse_mode: 'HTML',
+            reply_markup: buttons.reply_markup,
+        });
+    } catch (error) {
+        console.error("Error sending /books_class_10 message:", error);
+    }
 });
 
 module.exports = composer;
